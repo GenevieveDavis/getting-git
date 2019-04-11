95d5708e5ca59f574f69a3e0b99216ade58359a6
X10Khan <christin.khan@gmail.com>
2019-04-11 11:23:25 -0400

    mucking with Pace again

--- Pace_stuff.R
+++ Pace_stuff.R
@@ -1,5 +1,5 @@
 ## I will  some code
 p<-0.5
 N<-rpois(500, 12)
-Booger<- rbinom(500,N,p)   ### changing this name would be snotty
+Awesomesauce! <- rbinom(500,N,p)   ### changing this name would be snotty
 ## more comments
